3601120acec6fa3a7f531c74e061914118f6cf49
JayCammarano <jay.cammarano@gmail.com>
2020-08-29 19:13:26 -0400

    app.js out of sync with App.js

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-import React, { Fragment, useState } from "react";
-import CardModal from "./components/CardModal";
+import React from 'react';
+
 const App = () => {
-  const [selectedCard, setSelectedCard] = useState(null)
-  const setSelectedCardFunction = (taskNumber) => {
-    // search for CardModal with an id===taskNumber
-    // set className of matching CardModal === is active
-    if (selectedCard == null){
-    setSelectedCard(taskNumber)}
-    else{
-      setSelectedCard(null)
-    }
-  };
-  
   return (
-    <>
-      <div
-        className="card"
-        onClick={() => setSelectedCardFunction("task1")}
-      >
-        Click for Modal
-      </div>
-      
-      <CardModal id={selectedCard} selectedCard={selectedCard} setSelectedCard={setSelectedCard}/>
-    </>
-  );
+    <p>Hello from App component! Changes!</p>
+  )
 };
 
 export default App;
