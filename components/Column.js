d90678c48557128d48a21a9bb0e1c5454be6bb6b
JayCammarano <jay.cammarano@gmail.com>
2020-08-30 13:22:21 -0400

    got form to submit

--- components/Column.js
+++ components/Column.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import Task from './Task';
-
+import AddCard from "./addCard/AddCard"
 const Column = (props) => {
 
   const getItemStyle = (isDraggingOver, droppableStyle) => ({
@@ -31,6 +31,7 @@ const Column = (props) => {
           </div>
         )}
       </Droppable>
+      <AddCard />
     </div>
   )
 }
--- components/addCard/AddCard.js
+++ components/addCard/AddCard.js
@@ -1,28 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const AddCard = () => {
-  let cardFields = (
-    <div className="card">
-      <div className="card-header">
-        <p className="is-primary has-text-dark" onClick={expandCard}>
-          {" "}
-          ＋ Add a new task
-        </p>
-      </div>
-    </div>
-  );
-  const expandCard = () => {
-    cardFields = <input type="textarea" />;
+  const saveTask = () => {
+    console.log("hello");
   };
+  const expandCard = () => {
+    setCardFields(
+      <form onSubmit={saveTask} id="newTaskForm">
+        <div className="card-content">
+          <h6>Task Title</h6>
+          <input type="textarea"/>
+        </div>
+        <footer className="card-footer">
+        <input value="Save" type="submit" className="card-footer-item"/>
+
+        <input value="Cancel" type="submit" className="card-footer-item"/>
+
+        </footer>
+      </form>
+    );
+    
+  };
+  const [cardFields, setCardFields] = useState(
+    <p className="is-primary has-text-dark" onClick={expandCard}>
+      Add a new task
+    </p>
+  );
+
   return (
     <div className="card">
-      <div className="card-header">
-        <p className="is-primary has-text-dark" onClick={expandCard}>
-          {" "}
-          ＋ Add a new task
-        </p>
-        {cardFields}
-      </div>
+      <div className="card-header"></div>
+      {cardFields}
     </div>
   );
 };
